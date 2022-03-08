77081951edf84230ef31f13d234c517ea542f774
Martina-LP <madlp8@gmail.com>
2022-03-08 21:13:37 +0100

    Prepared Posts list

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -2,6 +2,23 @@
     <section>
         <div class="container">
             <h1>Posts</h1>
+            <div class="row row-cols-3">
+                <div v-for="post in posts" :key="post.id" class="col">
+                    <div class="card my-2">
+                        <!-- <img src="..." class="card-img-top" alt="..."> -->
+                        <div class="card-body">
+                            <h5 class="card-title">{{ post.title }}</h5>
+                            <p class="card-text">{{ truncateText(post.content, 40) }}</p>
+                        </div>
+                        <!-- <ul class="list-group list-group-flush">
+                            <li class="list-group-item">An item</li>
+                        </ul> -->
+                        <!-- <div class="card-body">
+                            <a href="#" class="card-link">Card link</a>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -21,6 +38,14 @@ export default {
             .then((response) => {
                 this.posts = response.data.results;
             });
+        },
+
+        truncateText: function(text, maxCharsNumber) {
+            if(text.length > maxCharsNumber) {
+                return text.substr(0, maxCharsNumber) + '...';
+            }
+
+            return text;
         }
     },
 
